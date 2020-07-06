879afbb8054d43679ca13c2b40b9e1a11e0b844a
Sebastian Gil Rodriguez <lbuggie.21@gmail.com>
2020-07-06 15:57:04 -0500

    Initialize store with initial values

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { createElement } from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import { createStore } from 'redux';
+import rootReducer from './reducers';
+import { Provider } from 'react-redux';
+
+let idGen = 0
+
+const initialState = [
+  { id: ++idGen, title: 'Book 1', category: 'Action' },
+  { id: ++idGen, title: 'Book 2', category: 'Learning' },
+  { id: ++idGen, title: 'Book 3', category: 'History' },
+]
+
+const store = createStore(rootReducer, { books: initialState })
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root'),
 );
