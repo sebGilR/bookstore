5c6a8c21f3019073cf640cff74191269e9226025
kalavhan <josue.brigido1@gmail.com>
2020-07-07 11:39:14 -0600

    Fix eslint errors

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -5,11 +5,13 @@ const CategoryFilter = ({ handleFilterChange }) => {
   const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
   return (
     <>
-      <label htmlFor="category-filter" id="category-filter-label">FILTER</label>
-      <select onChange={e => handleFilterChange(e)} id="category-filter">
-        <option key="ALL">ALL</option>
-        {categories.map(category => <option key={category}>{category}</option>)}
-      </select>
+      <label htmlFor="category-filter" id="category-filter-label">
+        FILTER
+        <select onChange={e => handleFilterChange(e)} id="category-filter">
+          <option key="ALL">ALL</option>
+          {categories.map(category => <option key={category}>{category}</option>)}
+        </select>
+      </label>
     </>
   );
 };
