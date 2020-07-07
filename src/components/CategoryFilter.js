dae3d77297f67b3143e35639af462f8f0db05d01
Sebastian Gil Rodriguez <lbuggie.21@gmail.com>
2020-07-07 14:57:04 -0500

    Fix prop validation

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -17,7 +17,7 @@ const CategoryFilter = ({ handleFilterChange }) => {
 };
 
 CategoryFilter.propTypes = {
-  handleFilterChange: PropType.objectOf(PropType.object).isRequired,
+  handleFilterChange: PropType.func.isRequired,
 };
 
 export default CategoryFilter;
